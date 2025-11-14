5301e7cec9e9898e1c5c5f6ef11680b60183c6e3
aniketshaw29 <aniketshawcoding@gmail.com>
2025-11-14 19:46:14 +0530

    health check

---
+++ src/main/java/com/sap/bankingapplication/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.sap.bankingapplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/health",
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**"
+                ).permitAll()   // 👈 allow health endpoint
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
+
---
+++ src/test/java/com/sap/bankingapplication/BankingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.bankingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
