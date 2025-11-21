d6b85ad6ba6f9cefd13efe1bb5bf5a16cb402bd7
Suryansh Pandey <suryansh.pandey@sap.com>
2025-11-21 13:01:21 +0530

    Edited comments only

--- src/main/java/com/sap/bankingapplication/BankingapplicationApplication.java
+++ src/main/java/com/sap/bankingapplication/BankingapplicationApplication.java
@@ -1,5 +1,7 @@
 package com.sap.bankingapplication;
 
+// File updated by Suryansh 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/sap/bankingapplication/OpenApiConfig.java
+++ src/main/java/com/sap/bankingapplication/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.sap.bankingapplication;
 
+// File updated by Suryansh 
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
